157faa045655484fc242de4240ce6b98b5782431
agbales <andrew.bales@substantial.com>
2019-08-28 09:31:53 -0700

    Individual detail edit

--- src/components/KittenDetails.tsx
+++ src/components/KittenDetails.tsx
@@ -2,6 +2,15 @@ import * as React from 'react';
 import { useState } from 'react';
 import styled from 'styled-components';
 
+const Button = styled.button`
+  background: transparent;
+  border-radius: 3px;
+  border: 2px solid palevioletred;
+  color: palevioletred;
+  padding: 0.25em 1em;
+  margin-left: 5px;
+`;
+
 const CatProp = styled.span`
   width: 100px;
   color: palevioletred;
@@ -14,13 +23,34 @@ const CatProp = styled.span`
 
 const KittenDetails = (props: any) => {
   let detail = props.detail;
-  const [isEditing, setIsEditing] = useState({ isEditing: false });
+  const [isEditing, setIsEditing] = useState(false);
+
+  const toggleDetailEdit = (): void => {
+    console.log('hit');
+    if (isEditing) {
+      setIsEditing(false);
+      console.log('editing false');
+    } else {
+      setIsEditing(true);
+      console.log('editing true');
+    }
+  };
 
   const KittenDisplay = (): JSX.Element => {
-    if (!isEditing) {
-      return <input value={detail} />;
+    if (isEditing) {
+      return (
+        <div>
+          <input value={detail} />
+          <Button onClick={() => toggleDetailEdit()}>Done</Button>
+        </div>
+      );
     } else {
-      return <div>{detail}</div>;
+      return (
+        <div>
+          <span>{detail}</span>
+          <Button onClick={() => toggleDetailEdit()}>Edit</Button>
+        </div>
+      );
     }
   };
 
